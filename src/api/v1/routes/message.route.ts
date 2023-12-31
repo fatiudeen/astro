0b90b3398c7ea1f87925edbf90bfe1090e570376
Shehu-Fatiudeen Lawal <shehufatiudeen@gmail.com>
2023-12-31 06:32:42 +0100

    updated messages

---
+++ src/api/v1/routes/message.route.ts
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-unresolved */
+import MessageController from '@controllers/message.controller';
+import { messageRequestDTO } from '@dtos/message.dto';
+import Route from '@routes/route';
+import { MessageInterface } from '@interfaces/Messages.Interface';
+
+export default class MessageRoute extends Route<MessageInterface> {
+  controller = new MessageController('message');
+  dto = messageRequestDTO;
+  initRoutes() {
+    this.router
+      .route('/')
+      .post(this.fileProcessor.uploadArray('attachment'), this.validator(this.dto.create), this.controller.create);
+    this.router.route('/:conversationId').get(this.validator(this.dto.id), this.controller.get);
+
+    return this.router;
+  }
+}
