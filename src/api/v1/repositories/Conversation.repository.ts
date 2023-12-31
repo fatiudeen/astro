3122c15d65c8e836b879b02832932b9cebf39b21
Shehu-Fatiudeen Lawal <shehufatiudeen@gmail.com>
2023-12-31 02:50:20 +0100

    updated messages

--- src/api/v1/repositories/Conversation.repository.ts
+++ src/api/v1/repositories/Conversation.repository.ts
@@ -4,4 +4,17 @@ import Repository from '@repositories/repository';
 
 export default class ConversationRepository extends Repository<ConversationInterface> {
   protected model = Conversation;
+
+  findRecipients(id: string) {
+    return new Promise<DocType<ConversationInterface>[]>((resolve, reject) => {
+      const q = this.model.find().in('recipients', [id]).sort({ updatedAt: -1 });
+      q.lean()
+        .then((r) => {
+          resolve(<DocType<ConversationInterface>[]>(<unknown>r));
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
 }
--- src/api/v1/repositories/Message.repository.ts
+++ src/api/v1/repositories/Message.repository.ts
@@ -4,4 +4,19 @@ import Repository from '@repositories/repository';
 
 export default class MessageRepository extends Repository<MessageInterface> {
   protected model = Message;
+
+  find(_query?: Partial<MessageInterface>) {
+    return new Promise<DocType<MessageInterface>[]>((resolve, reject) => {
+      let query: Record<string, any> = _query || {};
+
+      const q = this.model.find(query).populate('from to').sort({ createdAt: -1 });
+      q.lean()
+        .then((r) => {
+          resolve(<DocType<MessageInterface>[]>(<unknown>r));
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
 }
